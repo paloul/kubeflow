5a71042765ab30a60bbe77b4fe7677e218d8ab4a
George K. Paloulian <george.k.paloulian@gmail.com>
2021-06-27 22:57:03 -0700

    Added image tag to docker image

--- components/crud-web-apps/build_jupyter-web-app.sh
+++ components/crud-web-apps/build_jupyter-web-app.sh
@@ -1,7 +1,7 @@
 aws ecr get-login-password --region us-west-2 --profile bl-babylon | docker login --username AWS --password-stdin 562046374233.dkr.ecr.us-west-2.amazonaws.com
 
-docker build . -t ai.beyond/babylon/kubeflow/jupyter-web-app -f jupyter/Dockerfile --no-cache
+docker build . -t ai.beyond/babylon/kubeflow/jupyter-web-app:v1.3.0 -f jupyter/Dockerfile --no-cache
 
-docker tag ai.beyond/babylon/kubeflow/jupyter-web-app 562046374233.dkr.ecr.us-west-2.amazonaws.com/ai.beyond/babylon/kubeflow/jupyter-web-app
+docker tag ai.beyond/babylon/kubeflow/jupyter-web-app:v1.3.0 562046374233.dkr.ecr.us-west-2.amazonaws.com/ai.beyond/babylon/kubeflow/jupyter-web-app:v1.3.0
 
-docker push 562046374233.dkr.ecr.us-west-2.amazonaws.com/ai.beyond/babylon/kubeflow/jupyter-web-app
+docker push 562046374233.dkr.ecr.us-west-2.amazonaws.com/ai.beyond/babylon/kubeflow/jupyter-web-app:v1.3.0
